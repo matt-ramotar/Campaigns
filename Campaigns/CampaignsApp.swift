4521d01fa4ef9efdf29bfb4278fc564b5c62d411
Matt Ramotar <mramotar@dropbox.com>
2023-03-03 20:58:09 -0500

    Initial Commit

---
+++ Campaigns/CampaignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampaignsApp.swift
+//  Campaigns
+//
+//  Created by mramotar on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CampaignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
